3243e033d6f2a4c7645b77a8276d37a72884a673
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-03-15 19:27:30 -0400

    snake game

--- client.js
+++ client.js
@@ -13,6 +13,12 @@ const connect = function () {
   conn.on("data", (data) => {
     console.log(data);
   });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: SS5")
+  });
+
   return conn;
 };
 
